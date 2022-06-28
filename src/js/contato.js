b5a3bc282949f5343415b3d790a62c82078da238
Vinícius Pereira <viniciuspsb@gmail.com>
2022-06-27 22:15:17 -0300

    Ajustes finais

--- src/js/contato.js
+++ src/js/contato.js
@@ -12,27 +12,41 @@ btnEnviar.addEventListener("click", () => {
     validaCampos();
 });
 
+function emailValido(email) {
+    let valid =
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+            email
+        );
+
+    return !valid;
+}
+
 function validaNome() {
     if (nomeInput.value.length == 0) {
         nomeInput.classList.add("invalido");
         document.querySelector(".divNome .aviso").style.visibility = "visible";
+        return false;
     } else {
         nomeInput.classList.remove("invalido");
         document.querySelector(".divNome .aviso").style.visibility = "hidden";
+        return true;
     }
 }
 
 function ValidaEmail() {
-    if (emailInput.value.length == 0) {
+    if (emailInput.value.length == 0 || emailValido(emailInput.value)) {
         emailInput.classList.add("invalido");
         document.querySelector(".divEmail .aviso").style.visibility = "visible";
+        return false;
     } else {
         emailInput.classList.remove("invalido");
         document.querySelector(".divEmail .aviso").style.visibility = "hidden";
+        return true;
     }
 }
 
 function validaCampos() {
-    validaNome();
-    ValidaEmail();
+    let nomeValido = validaNome();
+    let emailValido = ValidaEmail();
+    if (nomeValido && emailValido) alert("Formulário enviado!");
 }
